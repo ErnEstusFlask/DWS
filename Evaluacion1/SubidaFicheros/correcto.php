107fe6049eff326c1cbf32831cb11b1a6d0067ab
Ernesto <ernesto.aunon09@gmail.com>
2018-10-26 16:43:39 +0200

    Cambio formulario

--- Evaluacion1/SubidaFicheros/correcto.php
+++
@@ -1,8 +0,0 @@
-<?php
-include ('bGeneral.php');
-cabecera ($_SERVER ['PHP_SELF']); //el archivo actual
-echo "Nombre:", $_GET['nombre'];
-echo '<br>';
-echo "Edad:", $_GET ['edad'];
-pie();
-?>
---
+++ Evaluacion1/SubidaFicheros/form_1.php
@@ -0,0 +1,9 @@
+<?php
+$name=$_REQUEST['nombre'];
+$age=$_REQUEST['edad'];
+$mail=$_REQUEST['correo'];
+echo "<b>Datos Filtrados:</b><br>";
+echo "Nombre: $name<br>";
+echo "Edad: $age<br>";
+echo "Email: $mail<br>";
+?>
